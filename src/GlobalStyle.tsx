d242d464848063c8ecd8738f6e2a762b00249f02
tranthaison1231 <thaison.coderstokyo@gmail.com>
2021-06-29 10:08:09 +0700

    Change emotion -> styled components

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,24 +1,17 @@
-import { Global, css, GlobalProps } from '@emotion/react';
+import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = (props: Omit<GlobalProps, 'styles'>) => {
-  return (
-    <Global
-      {...props}
-      styles={css`
-        html {
-          font-family: sans-serif;
-          -ms-text-size-adjust: 100%;
-          -webkit-text-size-adjust: 100%;
-          -webkit-font-smoothing: antialiased;
-          text-rendering: optimizeLegibility;
-        }
-        body {
-          margin: 0;
-          font-family: 'Roboto', 'HelveticaNeue', 'Helvetica Neue', sans-serif;
-        }
-      `}
-    />
-  );
-};
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: sans-serif;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+  }
+  body {
+    margin: 0;
+    font-family: 'Roboto', 'HelveticaNeue', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 export default GlobalStyle;
--- src/shared/components/common/LazyImage.tsx
+++ src/shared/components/common/LazyImage.tsx
@@ -1,5 +1,4 @@
-import { keyframes } from '@emotion/react';
-import styled from '@emotion/styled';
+import styled, { keyframes } from 'styled-components';
 import { useRef } from 'react';
 import LazyLoad from 'react-lazyload';
 
